dd23e565f0f2e421779a4acfbbb5d847563cc6a6
manobalan-08 <manobalan.m2023cse@sece.ac.in>
2024-11-12 20:50:26 +0530

    Added Entrypoint.java

--- src/main/java/com/wellsfargo/counselor/Entrypoint.java
+++ src/main/java/com/wellsfargo/counselor/Entrypoint.java
@@ -1,3 +1,4 @@
+
 package com.wellsfargo.counselor;
 
 import org.springframework.boot.SpringApplication;
@@ -7,8 +8,17 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Entrypoint {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Entrypoint.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Entrypoint.class, args);
 
+    Advisor advisor = new Advisor();
+    advisor.setAdvisorName("Manobalan M");
+    advisor.setPortfolios(50);
+    advisor.setUpdatePortfolio(10);
+
+    System.out.println("Hello! I am your financial advisor Mr.: " + advisor.getAdvisorName());
+    System.out.println("I have to create portfolios for my customers and I am glad in doing so: " + advisor.getPortfolios());
+    System.out.println("I have to update the portfolios: " + advisor.getUpdatePortfolio());
+    System.out.println("Removed the Portfolios of Ex-customers: " + advisor.decrementPortfolioCount());
+  }
 }
